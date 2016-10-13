4c0cc29cc19532e2c30992714fb8404687497749
StillsSma <samdawson301@live.com>
2016-10-13 13:54:16 -0400

    Finished Calculator

--- app/models.py
+++ app/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Calculator(models.Model):
-    num1 = models.CharField(max_length=20)
-    num2 = models.CharField(max_length=20)
+class Chirp(models.Model):
+    body = models.TextField(max_length=140)
+    created = models.DateTimeField(auto_now_add=True)
--- app/views.py
+++ app/views.py
@@ -1,10 +1,36 @@
 from django.shortcuts import render
+from app.models import Chirp
 
 # Create your views here.
 
+
 def index_view(request):
-    print(request.POST)
+    if request.GET:
 
+        num1 = request.GET['num1']
+        num2 = request.GET['num2']
+        operator = request.GET['operator']
+        try:
+            if operator == "+":
+                answer = (num1) + int(num2)
+            elif operator == "-":
+                answer = int(num1) - int(num2)
+            elif operator == "*":
+                answer = int(num1) * int(num2)
+            elif operator == "/":
+                if int(num2) == 0:
+                    answer = "ERROR"
+                else:
+                    answer = int(num1) / int(num2)
+        except ValueError:
+            Error = "ERROR"
+            answer = 0
+        context = {
+        "answer": answer,
+        "num1": num1,
+        "num2": num2,
+        "operator": operator,
+        }
+        return render(request, "index.html", context)
 
-
-    return render(request,"index.html")
+    return render(request, "index.html")
